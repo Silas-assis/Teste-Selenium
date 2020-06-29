ba58bff1cb12ebe5df1b449ba230d0672d01aa7d
Silas Assis <silaslima-19@hotmail.com>
2020-06-29 15:25:16 -0300

    URL a sert testada

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -1,4 +1,18 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteCadastro {
+	@Test
 
+	public void deveBuscarTextoNaPagina() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\assissl\\Desktop\\Curso Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize(); // Comando que deixa o navegador em tela cheia.
+		driver.get("file:///" + System.getProperty("user.dir") + 
+				"/src/main/resources/componentes.html");
+	
+		
+	}
 }
