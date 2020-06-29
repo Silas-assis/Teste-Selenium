753aaaed0b72b4a9ad877069518f1afc7330334e
Silas Assis <silaslima-19@hotmail.com>
2020-06-29 16:02:25 -0300

    Ajustando a linha de codigo referente ao Elemento de Escolaridade

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TesteCadastro {
 	@Test
@@ -17,7 +19,17 @@ public class TesteCadastro {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste"); //Input Nome.
 		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Testando"); // Input Sobrenome.
 		driver.findElement(By.id("elementosForm:sexo:0")).click(); //Escolhendo Sexo Masculino.
-		driver.findElement(By.id("elementosForm:comidaFavorita:1")).click(); // Escolhando Comida (Frango).
+		driver.findElement(By.id("elementosForm:comidaFavorita:1")).click(); // Escolhendo Comida (Frango).
 		
+		WebElement elementEscolaridade =  driver.findElement(By.id("elementosForm:escolaridade")); // Escolhendo Escolaridade.
+		Select comboEscolaridade = new Select(elementEscolaridade);
+		comboEscolaridade.selectByVisibleText("Superior"); 
+		
+		WebElement elementEsportes =  driver.findElement(By.id("elementosForm:esportes")); // Escolhendo Esportes.
+		Select comboEsportes = new Select(elementEsportes);
+		comboEsportes.selectByVisibleText("Corrida");
+		comboEsportes.selectByVisibleText("Futebol");
+		
+		driver.findElement(By.id("elementosForm:cadastrar")).click();//Botão para Cadastrar.
 	}
 }
