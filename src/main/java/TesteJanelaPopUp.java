82acc999e174a6ef5d359b93680f01dd5e8002fc
Silas Assis <silaslima-19@hotmail.com>
2020-07-01 14:58:23 -0300

    principal.

---
+++ src/main/java/TesteJanelaPopUp.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteJanelaPopUp {
+
+	@Test
+	public void popUp() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\assissl\\Desktop\\Curso Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize(); // Comando que deixa o navegador em tela cheia.
+		driver.get("file:///" + System.getProperty("user.dir") + 
+				"/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpEasy")).click();
+		driver.switchTo().window("Popup"); // Redirecionando para a Janela do PopUp 
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo!"); // Escrevendo no PopUp
+		driver.close(); // Fechar apenas a janela do PopUp
+		driver.switchTo().window(""); // Retornando para a Janela Principal.
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo também!");
+
+		driver.quit();
+	}
+}
