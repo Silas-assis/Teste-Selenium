508ca63456edd3f62a16a4169fefc2370f8103bf
Silas Assis <silaslima-19@hotmail.com>
2020-07-02 14:13:26 -0300

    Teste Janela PopUp Window Handles

--- src/main/java/TesteJanelaPopUp.java
+++ src/main/java/TesteJanelaPopUp.java
@@ -23,4 +23,24 @@ public class TesteJanelaPopUp {
 
 		driver.quit();
 	}
+	@Test
+	public void InteragirJanelaSemTitulo() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\assissl\\Desktop\\Curso Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize(); // Comando que deixa o navegador em tela cheia.
+		driver.get("file:///" + System.getProperty("user.dir") + 
+				"/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpHard")).click();
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getWindowHandles());
+		driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+		driver.findElement(By.tagName("textarea")).sendKeys("Testando no Pop-Up");
+		
+		driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+		driver.findElement(By.tagName("textarea")).sendKeys("Testando na Home");
+		
+		driver.quit();
+	}
 }
